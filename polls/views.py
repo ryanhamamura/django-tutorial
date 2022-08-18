b4bc6d650630c57dfc2cf7e3851587651841b892
Ryan Hamamura <rhamamur@gmail.com>
2022-08-18 13:53:17 -1000

    part 3: created a view that actually does something

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+    # return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
     return HttpResponse(f"You're looking at question {question_id}.")
